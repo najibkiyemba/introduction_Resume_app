277a84f1999afeffb7bb2e6951d5b670dd8d3163
Admin <admin@MacBook-Pro.local>
2023-07-26 00:33:10 -0700

    check deployment

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>dkfjjfiejfjdd</p>
+        <h2>Some times I play soccer</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
